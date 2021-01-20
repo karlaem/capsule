426b0462304bddccc3675ff57e44e62f396c9361
ayakade <ayakade13@gmail.com>
2021-01-19 18:54:07 -0800

    add class="nav"

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,7 +6,7 @@ import {
 
 export default function(){
     return(
-        <div>
+        <div class="nav">
             <Link to="/nearby">Nearby</Link> 
             <Link to="/capsule/new">New</Link> 
         </div>
